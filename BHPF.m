ae4e06ea8da58ac1989eb5d0e406d37f81c07c7a
Muhammad Habibi Husni <habibihusni18@gmail.com>
2024-10-23 15:22:38 +0700

    feat: add high pass filters

---
+++ BHPF.m
@@ -0,0 +1,52 @@
+function outImg = BHPF(img, D0, n)
+% IHPF Melakukan Butterworth high pass filter melalui ranah frekuensi
+%   D0  : frekuensi cutoff
+%   n   : orde butterworth
+    if nargin < 3
+        n = 1;
+    end
+
+    [N, M, C] = size(img);
+    P = 2*N;
+    Q = 2*M;
+    outImg = zeros(N,M,C);
+    for ch = 1:C
+        % Mengonversi citra channel ke tipe double dan mengskalakan ke range [0,1]
+        in = im2double(img(:,:,ch));
+        
+        % melakukan padding pada matriks kanal
+        inPadded = zeros(P,Q);
+        inPadded(1:N, 1:M) = in;
+        
+        % konversi ke bentuk FFT dan melakukan fftshift
+        F = fftshift(fft2(inPadded));
+
+        % Bangkit matriks yang menunjukkan jarak koordinat ke pust
+        u = 0:(P-1);
+        v = 0:(Q-1);
+        idx = find(u > P/2); 
+        u(idx) = u(idx) - P; 
+        idy = find(v > Q/2); 
+        v(idy) = v(idy) - Q; 
+        [V, U] = meshgrid(v, u);
+        D = sqrt(U.^2 + V.^2);
+
+        % Membuat mask BHPF dengan cutoff frekuensi D0 dan order n
+        H = 1./(1 + (D./D0).^(2*n)); 
+        H = fftshift(1-H);
+
+        G = H.*F; % aplikasikan filter kepada frekuensi citra
+
+        % lakukan inverse shift,inverse fft, mengambil bagian real frekuensi
+        % untuk mendapatkan citra hasil di ranah spatial
+        G = ifftshift(G);
+        outPadded = real(ifft2(G));
+        
+        % mendapatkan citra kanal hasil dengan membuang bagian padding
+        out = outPadded(1:N,1:M);
+
+        % masukkan hasil citra kanal ke variabel output
+        outImg(1:N,1:M,ch) = out;
+    end
+end
+
---
+++ IHPF.m
@@ -0,0 +1,47 @@
+function outImg = IHPF(img, D0)
+% IHPF Melakukan ideal high pass filter melalui ranah frekuensi
+%   D0 : frekuensi cutoff
+    [N, M, C] = size(img);
+    P = 2*N;
+    Q = 2*M;
+    outImg = zeros(N,M,C);
+    for ch = 1:C
+        % Mengonversi citra channel ke tipe double dan mengskalakan ke range [0,1]
+        in = im2double(img(:,:,ch));
+        
+        % melakukan padding pada matriks kanal
+        inPadded = zeros(P,Q);
+        inPadded(1:N, 1:M) = in;
+        
+        % konversi ke bentuk FFT dan melakukan fftshift
+        F = fftshift(fft2(inPadded));
+
+        % Bangkit matriks yang menunjukkan jarak koordinat ke pust
+        u = 0:(P-1);
+        v = 0:(Q-1);
+        idx = find(u > P/2); 
+        u(idx) = u(idx) - P; 
+        idy = find(v > Q/2); 
+        v(idy) = v(idy) - Q; 
+        [V, U] = meshgrid(v, u);
+        D = sqrt(U.^2 + V.^2);
+
+        % Membuat mask filter high-pass ideal dengan cutoff frekuensi D0
+        H = double(D <= D0);
+        H = fftshift(1-H);
+
+        G = H.*F; % aplikasikan filter kepada frekuensi citra
+
+        % lakukan inverse shift,inverse fft, mengambil bagian real frekuensi
+        % untuk mendapatkan citra hasil di ranah spatial
+        G = ifftshift(G);
+        outPadded = real(ifft2(G));
+        
+        % mendapatkan citra kanal hasil dengan membuang bagian padding
+        out = outPadded(1:N,1:M);
+
+        % masukkan hasil citra kanal ke variabel output
+        outImg(1:N,1:M,ch) = out;
+    end
+end
+
